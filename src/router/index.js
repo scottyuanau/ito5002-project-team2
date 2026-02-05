f51e3524ff300207edf39f83a2c4b1a6ed0d4727
Scott Yuan <scoworthyuan@gmail.com>
2026-02-06 08:15:32 +1100

    fixed 404 error when going forward or backward on browser

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
@@ -12,7 +12,7 @@ import KnowledgeView from '../views/KnowledgeView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
